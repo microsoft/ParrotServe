40e65221f6db2d8108b0b64c396ee16f4b107d3b
SiriusNEO <siriusneo@qq.com>
2023-12-19 08:59:25 +0000

    first commit

--- run_shared_prompt_exps.sh
+++ run_shared_prompt_exps.sh
@@ -1,18 +1,18 @@
 #/bin/bash
 
-echo 'mode, batch_size, sf_e2e, sf_model, dfg_e2e, dfg_model, total_e2e' > ./shared_prompt_exp_2.csv
-for mode in 'vllm_diverged' 'vllm_shared' 'parrot_shared'
-do
-	for batch_size in 8 16 32 64
-    do
-        python benchmark/microbench/bench_shared_prompt_e2e.py -m $mode -b $batch_size --use-sample
-    done
-done
+# echo 'mode, batch_size, sf_e2e, sf_model, dfg_e2e, dfg_model, total_e2e' > ./shared_prompt_exp_2.csv
+# for mode in 'vllm_diverged' 'vllm_shared' 'parrot_shared'
+# do
+# 	for batch_size in 8 16 32 64
+#     do
+#         python benchmark/microbench/bench_shared_prompt_e2e.py -m $mode -b $batch_size --use-sample
+#     done
+# done
 
 echo 'mode, batch_size, sf_e2e, sf_model, dfg_e2e, dfg_model, total_e2e' > ./shared_prompt_exp_1.csv
-for mode in 'vllm_shared' 'parrot_shared'
+for mode in 'vllm_diverged'
 do
-	for max_gen_length in 100 200 300 400 480
+	for max_gen_length in 800
     do
         python benchmark/microbench/bench_shared_prompt_e2e.py -m $mode -l $max_gen_length
     done
